4f49678c23c81001c497fb600dcce9e8bc985fec
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-05 16:41:20 +0900

    Title: 369 게임, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/369 게임/369-game.py
+++ 240205/369 게임/369-game.py
@@ -1,7 +1,20 @@
+# n = int(input())
+
+# for i in range(1,n+1):
+#     if i%3==0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
+#         print(0, end = ' ')
+#     else:
+#         print(i, end= ' ')
+
+
 n = int(input())
 
-for i in range(1,n+1):
-    if i%3==0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
-        print(0, end = ' ')
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print("0", end=" ")
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print("0", end=" ")
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+        print("0", end=" ")
     else:
-        print(i, end= ' ')
+        print(i, end = " ")
