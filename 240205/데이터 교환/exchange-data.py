a63a8c6394b53207ddd374ef2c64aabbfebbcea0
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-05 17:49:24 +0900

    Title: 데이터 교환, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/데이터 교환/exchange-data.py
+++ 240205/데이터 교환/exchange-data.py
@@ -1,8 +1,15 @@
+# a, b, c = 5, 6, 7
+# temp_c = c
+# c = b
+# b = a
+# a = temp_c
+
+# print(a)
+# print(b)
+# print(c)
+
 a, b, c = 5, 6, 7
-temp_c = c
-c = b
-b = a
-a = temp_c
+a, b, c = c, a, b
 
 print(a)
 print(b)
