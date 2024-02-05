6da3b36397df5fcaec042c1ad18160c943d18ede
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-05 16:56:50 +0900

    Title: 정수 선언하고 뺄셈, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240205/정수 선언하고 뺄셈/define-numbers-and-substract.py
@@ -0,0 +1,15 @@
+# a = "toy"
+# b = 3.5346
+# c = 2
+# print(a, type(a))
+# print(b, type(b))
+# print(c, type(c))
+
+# a = 5
+# b = 9
+# c = a + b 
+# print("c =",c)
+
+a = 97
+b = 13
+print(f"{a} - {b} = {a-b}")
