e9c366e66f7efad99e70b59413b53c1ba7617fd3
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-05 17:28:06 +0900

    Title: 달에서 무게 구하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240205/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,11 @@
+# a = 33.567268
+# print("{0:^10.2f}".format(a))
+# print(f"{a:.2f}")
+
+
+a = 13
+b = 0.165000
+
+# print("%d * %.6f = %.6f" % (a, b, a*b))
+# print("{0} * {1:.6f} = {2:.6f}".format(a, b, a*b))
+print(f"{a} * {b:.6f} = {a*b:.6f}")
