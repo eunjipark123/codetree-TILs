85843c42bb986b8042e02b733c4e010c14fd3c28
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-05 17:13:20 +0900

    Title: 세 정수형 변수 선언, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240205/세 정수형 변수 선언/declaration-of-three-natural-numbers.py
@@ -0,0 +1,37 @@
+# a = 5
+# print("A is %d" %a)
+
+# b = "apple"
+# print("B is %s" %b)
+# print("A is %d and B is %s" %(a, b))
+# print()
+
+# a, b = 5, "apple"
+# print("A is {0}".format(a))
+# print("A is {new_a}".format(new_a = a))
+
+# print("B is {0}".format(b))
+# print("B is {new_b}".format(new_b = b))
+# print()
+# print("A is {0} and B is {1}".format(a, b))
+# print("A is {1} and B is {0}".format(a, b))
+# print("B is {new_b} and A is {new_a}".format(new_a = a, new_b = b))
+
+# print()
+# print()
+
+# # f'string (python 3.6)
+# a, b = 5, "apple"
+
+# print(f"A is {a}")
+# print(f"B is {b}")
+# print(f"A is {a} and B is {b}")
+
+# a, b, c = 7, 23, 30
+# # 변수 포맷
+# print("%d + %d = %d" % (a , b, c))
+# # format
+# print("{0} + {1} = {2}".format(a, b, c))
+# print("{a} + {b} = {c}".format(a=a, b=b, c=c))
+# # fsting
+print(f"{a} + {b} = {c}")
