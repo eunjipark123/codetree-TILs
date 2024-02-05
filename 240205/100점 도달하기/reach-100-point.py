26b2379c775a5015e90f10e2fab60bbeeef720e9
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-05 16:00:21 +0900

    Title: 100점 도달하기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/100점 도달하기/reach-100-point.py
+++ 240205/100점 도달하기/reach-100-point.py
@@ -1,15 +1,29 @@
+# score = int(input())
+
+# while score <= 100:
+#     if score >= 90:
+#         print("A", end=" ")
+#     elif score >= 80:
+#         print("B", end=" ")
+#     elif score >= 70:
+#         print("C", end=" ")
+#     elif score >= 60:
+#         print("D", end=" ")
+#     else:
+#         print("F", end=" ")
+
+#     score = score + 1
+
 score = int(input())
 
-while score <= 100:
-    if score >= 90:
+for i in range(score, 101):
+    if i >= 90:
         print("A", end=" ")
-    elif score >= 80:
+    elif i>= 80:
         print("B", end=" ")
-    elif score >= 70:
+    elif i>= 70:
         print("C", end=" ")
-    elif score >= 60:
+    elif i>= 60:
         print("D", end=" ")
     else:
         print("F", end=" ")
-
-    score = score + 1
