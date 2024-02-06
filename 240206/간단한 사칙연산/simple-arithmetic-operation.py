646d7ecfdb0e3acc2b544a837f26a6b6b471c5c1
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-06 10:50:04 +0900

    Title: 간단한 사칙연산, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240206/간단한 사칙연산/simple-arithmetic-operation.py
@@ -0,0 +1,13 @@
+# 사칙연산
+
+# a, b = 9, 4
+# print(a + b, a * b, a - b, a // b, a % b, a / b, a * b)
+
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(a + b)
+print(a - b)
+print(a // b)
+print(a % b)
