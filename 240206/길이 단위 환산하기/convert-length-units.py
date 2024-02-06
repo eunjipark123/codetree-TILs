da536ec66ea66519884f89e51236946f872460ae
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-06 10:15:50 +0900

    Title: 길이 단위 환산하기, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/길이 단위 환산하기/convert-length-units.py
+++ 240206/길이 단위 환산하기/convert-length-units.py
@@ -1,4 +1,8 @@
 # 1ft = 30.48cm
 # ft를 입력 받고, cm으로 환산해서, 첫째자리까지 출력
-a = float(input()) * 30.48
-print(f"{a:.1f}")
+# a = float(input()) * 30.48
+# print(f"{a:.1f}")
+
+a = float(input())
+
+print(f"{a*30.48:.1f}")
