a31ebeff585c4ece1c89451519866ed473b241d2
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-06 10:21:25 +0900

    Title: 입력받아 계산 2, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240206/입력받아 계산 2/input-calculate-2.py
@@ -0,0 +1,17 @@
+# 공백을 사이에 두고 입력
+# split() 함수를 이용한다. 
+
+# a = input()
+# # 리스트 형태로 출력
+# print(a.split())
+
+# arr = [5, 6, 10]
+# print(f"First element is arr[0]")
+# print(f"Second element is arr[1]")
+# print(f"Last element is arr[2]")
+
+# print(f"{a[0]}은(는) 첫 번째로 입력된 녀석입니다. ")
+
+a = input().split()
+multi = int(a[0]) * int(a[1])
+print(multi)
