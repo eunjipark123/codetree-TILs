9863ec23856b4ba786be26bd0c9710887f68bb1d
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-06 10:08:34 +0900

    Title: 입력받아 계산, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240206/입력받아 계산/input-calculate.py
@@ -0,0 +1,18 @@
+# a = input()
+
+# # 문자열을 입력 받는 경우
+# # print(f"A is {a}")
+
+# # 숫자를 입력받는 경우 (정수, 실수) -- 에러 발생 케이스
+# # print(a)
+# # print(a + 1)
+
+# # 숫자 입력 받는 경우 (에러를 없애려면 )-- 형 변환
+# a = int(a) 
+# print(a+1)
+
+# a = int(input())
+
+a = input()
+a = int(a)
+print(a+2)
