786c24c1c4e3922048ff14339078a62760b02603
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-06 11:19:03 +0900

    Title: 세 수의 합과 평균 구하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
+++ 240206/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -3,5 +3,8 @@ a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
 
-arr = [a, b, c]
-print(f"{sum(arr)}\n{sum(arr) // len(arr)}")
+# arr = [a, b, c]
+# print(f"{sum(arr)}\n{sum(arr) // len(arr)}")
+
+print(a + b + c)
+print((a + b + c) // 3)
