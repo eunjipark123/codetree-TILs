1c42cd885ec1ef66a9cff80198f9105fa1800cbe
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-10 22:21:28 +0900

    Title: 비교 연산, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240210/비교 연산/comparison-operator.py
+++ 240210/비교 연산/comparison-operator.py
@@ -2,32 +2,40 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-if a >= b:
-    print(1)
-else:
-    print(0)
+# if a >= b:
+#     print(1)
+# else:
+#     print(0)
 
-if a > b:
-    print(1)
-else:
-    print(0)
+# if a > b:
+#     print(1)
+# else:
+#     print(0)
 
-if b >= a:
-    print(1)
-else:
-    print(0)
+# if b >= a:
+#     print(1)
+# else:
+#     print(0)
 
-if b > a:
-    print(1)
-else:
-    print(0)
+# if b > a:
+#     print(1)
+# else:
+#     print(0)
 
-if a == b:
-    print(1)
-else:
-    print(0)
+# if a == b:
+#     print(1)
+# else:
+#     print(0)
 
-if a != b:
-    print(1)
-else:
-    print(0)
+# if a != b:
+#     print(1)
+# else:
+#     print(0)
+
+
+print(int(a >= b))
+print(int(a > b))
+print(int(a <= b))
+print(int(a < b))
+print(int(a == b))
+print(int(a != b))
