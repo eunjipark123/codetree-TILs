f6de825676b065176074a5cdc11568d43a3d9e0a
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-11 18:18:35 +0900

    Title: 코로나 메뉴얼, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240211/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,21 @@
+arr_a = input().split()
+arr_b = input().split()
+arr_c = input().split()
+
+a_sym, a_temp = arr_a[0], int(arr_a[1])
+b_sym, b_temp = arr_b[0], int(arr_b[1])
+c_sym, c_temp = arr_c[0], int(arr_c[1])
+
+p = 0
+
+if a_sym == "Y" and a_temp >= 37:
+    p += 1
+if b_sym == "Y" and b_temp >= 37:
+    p += 1    
+if c_sym == "Y" and c_temp >= 37:
+    p += 1        
+
+if p >= 2:
+    print("E")
+else:
+    print("N")
