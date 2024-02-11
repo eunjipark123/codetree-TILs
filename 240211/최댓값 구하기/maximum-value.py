5ec0cc3bb73e171046b5ca8b677f58d75b5e3acd
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-11 18:13:54 +0900

    Title: 최댓값 구하기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240211/최댓값 구하기/maximum-value.py
+++ 240211/최댓값 구하기/maximum-value.py
@@ -1,9 +1,20 @@
 arr = input().split()
 a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
 
-if a >= b and a >= c:
-    print(a)
-elif b >= c and b >= a:
-    print(b)
+# if a >= b and a >= c:
+#     print(a)
+# elif b >= c and b >= a:
+#     print(b)
+# else:
+#     print(c)
+
+if a > b:
+    if a > c:
+        print(a)
+    else:
+        print(c)
 else:
-    print(c)
+    if b > c:
+        print(b)
+    else:
+        print(c)
