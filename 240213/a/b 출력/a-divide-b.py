a96214d0820798053f17e75c1c15649b76b5a97f
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-13 23:26:34 +0900

    Title: a/b 출력, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240213/a/b 출력/a-divide-b.py
@@ -0,0 +1,21 @@
+arr = input().split()
+a, b = int(arr[0]), int(arr[1])
+
+# 이상하게 나옴
+# print(f"{a/b:.20f}")
+
+d = a % b * 10
+result = 0
+result2 = "0."
+lists = []
+
+for _ in range(20):
+    c = d // b
+    result += 0.1 * c
+    lists.append(str(c))
+    d = d % b * 10
+
+for i in lists:
+    result2 += i
+
+print(result2)
