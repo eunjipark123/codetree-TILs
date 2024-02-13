9ee09fc1314e0949ca60f06c85ff714cb06c86ac
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-14 00:26:44 +0900

    Title: 청소당번 정하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240214/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,15 @@
+cnt_toilet = 0
+cnt_hall = 0
+cnt_class = 0
+
+n = int(input())
+
+for i in range(2, n+1):
+    if i % 12 == 0:
+        cnt_toilet += 1
+    elif i % 3 == 0:
+        cnt_hall += 1
+    elif i % 2 == 0:
+        cnt_class += 1
+
+print(cnt_class, cnt_hall, cnt_toilet)
