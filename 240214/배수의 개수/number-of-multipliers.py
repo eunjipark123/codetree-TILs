a2555bd7f14bf9880b076e098a3aa926d7b45fb9
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-14 00:20:16 +0900

    Title: 배수의 개수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240214/배수의 개수/number-of-multipliers.py
+++ 240214/배수의 개수/number-of-multipliers.py
@@ -1,14 +1,25 @@
 cnt_3 = 0
 cnt_5 = 0
 
+# 지저분한 방법이군..! 
+# for _ in range(10):
+#     n = int(input())
+#     if n % 3 == 0 and n % 5 == 0:
+#         cnt_3 += 1
+#         cnt_5 += 1
+#     elif n % 3 == 0:
+#         cnt_3 += 1
+#     elif n % 5 == 0:
+#         cnt_5 += 1
+
+# print(cnt_3, cnt_5)
+    
 for _ in range(10):
     n = int(input())
-    if n % 3 == 0 and n % 5 == 0:
+    if n % 3 == 0:
         cnt_3 += 1
+    # 독립되어 작용하므로 이렇게 써도 되는구나.. 
+    if n % 5 == 0:
         cnt_5 += 1
-    elif n % 3 == 0:
-        cnt_3 += 1
-    elif n % 5 == 0:
-        cnt_5 += 1
-
+    
 print(cnt_3, cnt_5)
