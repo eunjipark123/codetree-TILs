a7e8181365a036d2ec35d73ad8503d831b312a8b
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-15 00:28:36 +0900

    Title: 1까지 나누기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/1까지 나누기/divide-by-1.py
+++ 240215/1까지 나누기/divide-by-1.py
@@ -1,8 +1,15 @@
 n = int(input())
-share = n
+# share = n
 
-for i in range(1, n + 1):
-    if share // i <= 1:
-        print(i)
-        break
-    share //= i
+# for i in range(1, n + 1):
+#     if share // i <= 1:
+#         print(i)
+#         break
+#     share //= i 
+
+i = 1
+while n > 1:
+    n //= i
+    i += 1
+
+print(i-1)
