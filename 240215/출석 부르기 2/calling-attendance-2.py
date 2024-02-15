cfae5698a79aa2d7dfebb6984d6c0e5b2401789a
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-15 14:42:03 +0900

    Title: 출석 부르기 2, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/출석 부르기 2/calling-attendance-2.py
+++ 240215/출석 부르기 2/calling-attendance-2.py
@@ -1,11 +1,27 @@
-pre = {1: 'John', 2: 'Tom', 3: 'Paul', 4: 'Sam'}
+# pre = {1: 'John', 2: 'Tom', 3: 'Paul', 4: 'Sam'}
+
+# while True:
+#     n = int(input())
+    
+#     if n in pre.keys():
+#         print(f"{pre[n]}")
+
+#     else:
+#         print("Vacancy")    
+#         break
+
 
 while True:
     n = int(input())
-    
-    if n in pre.keys():
-        print(f"{pre[n]}")
 
+    if n == 1:
+        print("John")
+    elif n == 2:
+        print("Tom")
+    elif n == 3:
+        print("Paul")
+    elif n == 4:
+        print("Sam")
     else:
-        print("Vacancy")    
+        print("Vacancy")
         break
