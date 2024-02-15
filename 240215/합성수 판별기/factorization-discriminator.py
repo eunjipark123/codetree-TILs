35f684a89f90a5cee2edb2d297032aca104f6e51
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-15 15:51:29 +0900

    Title: 합성수 판별기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/합성수 판별기/factorization-discriminator.py
@@ -0,0 +1,11 @@
+# 합성수 : 1보다 커야 하고, 소수가 아니어야 함
+
+n = int(input())
+exists = False
+
+for i in range(2, n):
+    if n % i > 0:
+        exists = True
+        break
+
+print("C") if exists == True else print("N")
