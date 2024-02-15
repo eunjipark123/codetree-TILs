5fd04823311950c8fc69d3af7f7a5a206c4340eb
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-15 23:22:11 +0900

    Title: 두 개의 직각삼각형, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/두 개의 직각삼각형/two-right-triangle.py
+++ 240215/두 개의 직각삼각형/two-right-triangle.py
@@ -5,8 +5,11 @@ for i in range(n):
     for _ in range(n - i):
         print("*", end = "")
     # 빈칸 만들어주고 
-    for _ in range(i):
-        print(" "*2, end = "")
+    # i에 2를 곱하는 게 더 낫겠다. 
+    # for _ in range(i):
+    #     print(" "*2, end = "")
+    for _ in range(2 * i):
+        print(" ", end = "")
     # 다시 동일 수만큼 반복
     for _ in range(n - i):
         print("*", end = "")
