8f6f396a2418a2b288b592d642060be84d3c1818
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-15 17:26:12 +0900

    Title: 별표 출력하기 4, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/별표 출력하기 4/print-star-4.py
+++ 240215/별표 출력하기 4/print-star-4.py
@@ -1,11 +1,21 @@
 n = int(input())
 
-for i in range(n):
-    for _ in range(n - i):
+# for i in range(n):
+#     for _ in range(n - i):
+#         print("*", end = " ")
+#     print()
+
+# for i in range(n-2, -1, -1):
+#     for _ in range(n - i):
+#         print("*", end = " ")
+#     print()
+
+for i in range(n-1, -1, -1):
+    for _ in range(i + 1):
         print("*", end = " ")
     print()
 
-for i in range(n-2, -1, -1):
-    for _ in range(n - i):
+for i in range(1, n):
+    for _ in range(i + 1):
         print("*", end = " ")
     print()
