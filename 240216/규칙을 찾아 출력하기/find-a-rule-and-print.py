e6db9600dbaf72ccf4850556f4bd135cb3fa362d
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-16 18:23:00 +0900

    Title: 규칙을 찾아 출력하기, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240216/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == 4 or j == 0 or j == 4:
+            print("*", end = " ")
+        else:
+            if i > j:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+    print()
