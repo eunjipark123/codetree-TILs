62c24b2f64744f7db346bebcd3b70f504d7b63b6
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-16 18:30:12 +0900

    Title: 행 단위로 반복하여 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240216/행 단위로 반복하여 출력/print-in-row.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        print(j + 1, end = "")
+    print()
+
+# for i in range(2):
+#     for j in range(5):
+#         print(j + 1, end = "")
+#     print()
