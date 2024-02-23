339fb3ca3fcbcf05718cd38cd4802354c6902099
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-23 11:35:52 +0900

    Title: 좌우 지그재그 출력, Time: 154ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240223/좌우 지그재그 출력/print-lr-zigzag.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    if i % 2 == 0:
+        cnt_0 = n * i + 1
+        for _ in range(n):
+            print(cnt_0, end = " ")
+            cnt_0 += 1
+    else:
+        cnt_1 = n * (i + 1)
+        for _ in range(n):
+            print(cnt_1, end = " ")
+            cnt_1 -= 1
+    print()
