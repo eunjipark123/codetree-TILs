740a4048409ae755498bbc713a57ada11be6fd89
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-23 11:45:58 +0900

    Title: 행에 따라 다르게 출력, Time: 178ms, Memory: 26MB, Status: Passed - CodeTree

--- 240223/행에 따라 다르게 출력/output-differently-for-row.py
+++ 240223/행에 따라 다르게 출력/output-differently-for-row.py
@@ -1,12 +1,22 @@
 n = int(input())
 cnt = 0
+# for i in range(n):
+#     if i % 2 == 0:
+#         for _ in range(n):
+#             cnt += 1
+#             print(cnt, end = " ") 
+#     else:
+#         for _ in range(n):
+#             cnt += 2
+#             print(cnt, end = " ")
+#     print()
+
+# 묶을 수 있는 건 묶어서 표현하기 (간단하게)
 for i in range(n):
-    if i % 2 == 0:
-        for _ in range(n):
+    for _ in range(n):
+        if i % 2 == 0:
             cnt += 1
-            print(cnt, end = " ") 
-    else:
-        for _ in range(n):
+        else:
             cnt += 2
-            print(cnt, end = " ")
+        print(cnt, end = " ")
     print()
