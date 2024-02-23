aa1bb64b36350b75ab604fa583431ffe63fc66c4
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-23 11:17:26 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240223/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
+++ 240223/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
@@ -1,10 +1,20 @@
 n = int(input())
-cnt = 1
+# cnt = 1
+
+# for _ in range(n):
+#     for _ in range(n):
+#         print(2 * cnt, end = " ")
+#         cnt += 1
+#         if cnt >= 5:
+#             cnt = 1
+#     print()
+
+cnt = 2
 
 for _ in range(n):
     for _ in range(n):
-        print(2 * cnt, end = " ")
+        print(cnt, end = " ")
         cnt += 1
-        if cnt >= 5:
-            cnt = 1
+        if cnt == 10:
+            cnt = 2
     print()
