e07e74a7b7c40a3d517be2ff0ba425548ea2ec65
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 22:17:11 +0900

    Title: 배열의 특정 위치 찾기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240225/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,25 +1,41 @@
 arr = list(map(int, input().split()))
 
-# 첫 번째 방법
-sum_val_even = 0
-sum_val_three = 0
+# 두 번재 방법
+sum_even = sum(arr[1:10:2])
+
+sum_three = 0
 cnt_three = 0
-# even = []
-# three = []
-n = len(arr)
 
-for i in range(1, n, 2):
-    sum_val_even += arr[i]
-    # even.append(arr[i])
-
-for j in range(2, n, 3):
-    sum_val_three += arr[j]
+for i in arr[2:10:3]:
+    sum_three += i
     cnt_three += 1
-    # three.append(arr[j])
 
-avg_three = sum_val_three / cnt_three
+avg_three = sum_three / cnt_three
 
-print(f"{sum_val_even} {avg_three:.1f}")
+print(f"{sum_even} {avg_three:.1f}")
+
+
+
+# # 첫 번째 방법
+# sum_val_even = 0
+# sum_val_three = 0
+# cnt_three = 0
+# # even = []
+# # three = []
+# n = len(arr)
+
+# for i in range(1, n, 2):
+#     sum_val_even += arr[i]
+#     # even.append(arr[i])
+
+# for j in range(2, n, 3):
+#     sum_val_three += arr[j]
+#     cnt_three += 1
+#     # three.append(arr[j])
+
+# avg_three = sum_val_three / cnt_three
+
+# print(f"{sum_val_even} {avg_three:.1f}")
 # print(even)
 # print(three)
 
