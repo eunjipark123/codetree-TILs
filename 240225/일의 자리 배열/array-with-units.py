7bb8ab9b6a3f1fbab1424765fe34503e04f44b9f
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 23:36:02 +0900

    Title: 일의 자리 배열, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/일의 자리 배열/array-with-units.py
+++ 240225/일의 자리 배열/array-with-units.py
@@ -2,8 +2,19 @@ arr = list(map(int, input().split()))
 
 # print(arr)
 
-for i in range(2, 10):
-    arr.append((arr[i-2] + arr[i-1])%10)
+# for i in range(2, 10):
+#     arr.append((arr[i-2] + arr[i-1])%10)
 
-for i in arr:
+# for i in arr:
+#     print(i, end = " ")
+
+
+arr_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+arr_list[0] = arr[0]
+arr_list[1] = arr[1]
+
+for i in range(2, 10):
+    arr_list[i] = (arr_list[i - 1] + arr_list[i - 2]) % 10
+
+for i in arr_list:
     print(i, end = " ")
