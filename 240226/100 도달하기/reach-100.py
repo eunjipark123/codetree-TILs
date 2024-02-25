2de1436a2650af884ed59d2946596bd206ae895d
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 00:10:26 +0900

    Title: 100 도달하기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/100 도달하기/reach-100.py
+++ 240226/100 도달하기/reach-100.py
@@ -1,13 +1,25 @@
 n = int(input())
 
-arr = []
-arr.append(1)
-arr.append(n)
+# arr = []
+# arr.append(1)
+# arr.append(n)
 
-for i in range(2, 100):
-    if arr[-1] > 100:
+# for i in range(2, 100):
+#     if arr[-1] > 100:
+#         break
+#     arr.append(arr[i - 1] + arr[i - 2])
+
+# for i in arr:
+#     print(i, end = " ")
+    
+arr = [1, n]
+cnt = 1
+
+while True:
+    cnt += 1
+    arr.append(arr[cnt -1] + arr[cnt - 2])
+    if arr[cnt] > 100:
         break
-    arr.append(arr[i - 1] + arr[i - 2])
 
-for i in arr:
-    print(i, end = " ")
+for elem in arr:
+    print(elem, end = " ")
