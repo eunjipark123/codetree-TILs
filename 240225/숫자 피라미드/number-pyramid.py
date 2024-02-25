e45d8fc92a9264c57ba2c534e73ad3e3c26e688b
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 12:46:22 +0900

    Title: 숫자 피라미드, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240225/숫자 피라미드/number-pyramid.py
+++ 240225/숫자 피라미드/number-pyramid.py
@@ -1,6 +1,12 @@
 n = int(input())
 
-for i in range(n):
-    for j in range(i + 1):
-        print(i + 1, end = " ")
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(i + 1, end = " ")
+#     print()
+
+
+for i in range(1, n + 1):
+    for _ in range(i):
+        print(i, end = " ")
     print()
