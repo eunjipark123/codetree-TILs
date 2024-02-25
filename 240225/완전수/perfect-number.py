f0474bd9a9a1f8c3e0b7d4310d56e4f68759b37a
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 18:19:02 +0900

    Title: 완전수, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240225/완전수/perfect-number.py
@@ -0,0 +1,15 @@
+arr = input().split()
+start, end = int(arr[0]), int(arr[1])
+
+cnt = 0
+
+for i in range(start, end + 1):
+    sum_val = 0
+    for j in range(1, i):
+        if i % j == 0:
+            sum_val += j
+
+    if sum_val == i:
+        cnt += 1
+    
+print(cnt)
