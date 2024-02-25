90cef01990b74aa4ffd96f85900d51a9929b93f0
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 21:21:30 +0900

    Title: 학점 계산기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/학점 계산기/credit-calculator.py
+++ 240225/학점 계산기/credit-calculator.py
@@ -2,16 +2,28 @@ n = int(input())
 arr = map(float, input().split())
 sum_val = 0
 
-for i in arr:
-    sum_val += i
+# for i in arr:
+#     sum_val += i
 
+# avg = sum_val / n
+# if avg >= 4.0:
+#     grade = "Perfect"
+# elif avg >= 3.0:
+#     grade = "Good"
+# else:
+#     grade = "Poor"
+
+# print(f"{avg:.1f}")
+# print(grade)
+
+sum_val = sum(arr)
 avg = sum_val / n
-if avg >= 4.0:
-    grade = "Perfect"
-elif avg >= 3.0:
-    grade = "Good"
-else:
-    grade = "Poor"
 
 print(f"{avg:.1f}")
-print(grade)
+
+if avg >= 4.0:
+    print("Perfect")
+elif avg >= 3.0:
+    print("Good")
+else:
+    print("Poor")
