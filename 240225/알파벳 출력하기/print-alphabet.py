db634d6f9508d2c0b10caa95e6231ab6d60c9ecf
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 15:42:11 +0900

    Title: 알파벳 출력하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/알파벳 출력하기/print-alphabet.py
+++ 240225/알파벳 출력하기/print-alphabet.py
@@ -4,5 +4,8 @@ char = 'A'
 for i in range(n):
     for _ in range(i + 1):
         print(char, end = "")
-        char = chr(ord(char) + 1)
+        if char == 'Z':
+            char = 'A'
+        else:
+            char = chr(ord(char) + 1)
     print()
