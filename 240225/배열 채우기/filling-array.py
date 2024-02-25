340c6a602f00d20eb7de7352bb21721fbbf40249
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 21:37:18 +0900

    Title: 배열 채우기, Time: 187ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/배열 채우기/filling-array.py
+++ 240225/배열 채우기/filling-array.py
@@ -1,11 +1,21 @@
 arr = list(map(int, input().split()))
 arr_lists = []
 
-for i in arr:
-    if i == 0:
-        break
-    else:
-        arr_lists.append(i)
+# for i in arr:
+#     if i == 0:
+#         break
+#     else:
+#         arr_lists.append(i)
 
-for i in arr_lists[::-1]:
-    print(i, end = " ")
+# for i in arr_lists[::-1]:
+#     print(i, end = " ")
+
+cnt = 0
+for el in arr:
+    if el == 0:
+        break
+    cnt += 1
+
+# print(cnt)
+for i in range(cnt -1, -1, -1):
+    print(arr[i], end = " ")
