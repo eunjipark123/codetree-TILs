a01fe60ffb37a69d48fab62ad5d0b23168d94bf7
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 23:54:11 +0900

    Title: 숫자들의 배수, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/숫자들의 배수/multiple-of-numbers.py
+++ 240225/숫자들의 배수/multiple-of-numbers.py
@@ -1,14 +1,33 @@
+# n = int(input())
+# cnt = 0
+# num = 0
+# arr = []
+
+
+# while cnt < 2:
+#     num += n
+#     arr.append(num)
+#     if num % 5 == 0:
+#         cnt += 1
+
+# for i in arr:
+#     print(i, end = " ")
+    
+    
+
+
 n = int(input())
 cnt = 0
-num = 0
 arr = []
+arr.append(n)
 
+for i in range(1, 10):
+    a = arr[i - 1] + arr[0]
+    arr.append(a)
 
-while cnt < 2:
-    num += n
-    arr.append(num)
-    if num % 5 == 0:
+for elem in arr:
+    print(elem, end = " ")
+    if elem % 5 == 0:
         cnt += 1
-
-for i in arr:
-    print(i, end = " ")
+    if cnt >= 2:
+        break
