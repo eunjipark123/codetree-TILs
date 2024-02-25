44dc057c93afa022771cc066e90fa3df3f738b54
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 22:39:34 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240225/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
+++ 240225/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -1,10 +1,20 @@
 arr = list(map(int, input().split()))
 
-odds = sum(arr[0:10:2])
-even = sum(arr[1:10:2])
+# odds = sum(arr[0:10:2])
+# even = sum(arr[1:10:2])
 
-if odds >= even:
-    print(odds - even)
+# if odds >= even:
+#     print(odds - even)
 
-else:
-    print(even - odds)
+# else:
+#     print(even - odds)
+
+sum_even = 0
+sum_odds = 0
+for i in range(9):
+    if (i + 1) % 2 == 0:
+        sum_even += arr[i]
+    else:
+        sum_odds += arr[i]
+
+print(abs(sum_even - sum_odds))
