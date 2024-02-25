9d260729443dffe6eb18a7fac796b3101e28c6cd
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 01:19:26 +0900

    Title: 코로나 메뉴얼2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240226/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,19 @@
+arr_cnt = [0] * 4
+
+for _ in range(3):
+    arr = input().split()
+    if arr[0] == 'Y' and int(arr[1]) >= 37:
+        arr_cnt[0] += 1
+    elif arr[0] == 'N' and int(arr[1]) >= 37:
+        arrn_cnt[1] += 1
+    elif arr[0] == 'Y' and int(arr[1]) < 37:
+        arr_cnt[2] += 1
+    else:
+        arr_cnt[3] += 1
+
+
+for i in arr_cnt:
+    print(i, end = " ")
+
+if arr_cnt[0] >= 2:
+    print('E')
