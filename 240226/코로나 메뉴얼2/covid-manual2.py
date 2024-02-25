b15a3682d0792ea4e3227c62a79a283fd9b74658
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 01:31:15 +0900

    Title: 코로나 메뉴얼2, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/코로나 메뉴얼2/covid-manual2.py
+++ 240226/코로나 메뉴얼2/covid-manual2.py
@@ -1,19 +1,39 @@
 arr_cnt = [0] * 4
 
 for _ in range(3):
-    arr = input().split()
-    if arr[0] == 'Y' and int(arr[1]) >= 37:
-        arr_cnt[0] += 1
-    elif arr[0] == 'N' and int(arr[1]) >= 37:
-        arr_cnt[1] += 1
-    elif arr[0] == 'Y' and int(arr[1]) < 37:
-        arr_cnt[2] += 1
+    s, t = input().split()
+    t = int(t)
+
+    if t >= 37 and s == 'Y':
+        type_num = 0;
+    elif t >= 37 and s == 'N':
+        type_num = 1;
+    elif s == 'Y':
+        type_num = 2;
     else:
-        arr_cnt[3] += 1
+        type_num = 3;
 
+    arr_cnt[type_num] += 1
 
-for i in arr_cnt:
-    print(i, end = " ")
+for i in range(4):
+    print(arr_cnt[i], end = " ")
 
 if arr_cnt[0] >= 2:
-    print('E')
+    print("E")
+# for _ in range(3):
+#     arr = input().split()
+#     if arr[0] == 'Y' and int(arr[1]) >= 37:
+#         arr_cnt[0] += 1
+#     elif arr[0] == 'N' and int(arr[1]) >= 37:
+#         arr_cnt[1] += 1
+#     elif arr[0] == 'Y' and int(arr[1]) < 37:
+#         arr_cnt[2] += 1
+#     else:
+#         arr_cnt[3] += 1
+
+
+# for i in arr_cnt:
+#     print(i, end = " ")
+
+# if arr_cnt[0] >= 2:
+#     print('E')
