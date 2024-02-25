8a33f5c8f45be516739e805045d59935af81fab0
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-25 18:36:25 +0900

    Title: 소수 구하기, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240225/소수 구하기/get-prime.py
+++ 240225/소수 구하기/get-prime.py
@@ -1,10 +1,24 @@
 n = int(input())
 
+# for i in range(1, n + 1):
+#     cnt = 0
+#     for j in range(2, i):
+#         if i % j == 0:
+#             cnt += 1
+    
+#     if cnt == 0 and i != 1:
+#         print(i, end = " ")
+    
+
 for i in range(1, n + 1):
-    cnt = 0
+    if i == 1:
+        continue
+    
+    isprime = True
+
     for j in range(2, i):
         if i % j == 0:
-            cnt += 1
-    
-    if cnt == 0 and i != 1:
-        print(i, end = " ")
+            isprime = False
+        
+    if isprime:
+        print(i, end =" ")
