d7ee67e26f90598e5dd56ed005986a762b3cbcc0
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 22:26:16 +0900

    Title: 배열의 합, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240226/배열의 합/sum-of-array.py
@@ -0,0 +1,13 @@
+n = 4
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+for row in arr:
+    sum_val = 0
+    for i in row:
+        sum_val += i
+    
+    print(sum_val)
