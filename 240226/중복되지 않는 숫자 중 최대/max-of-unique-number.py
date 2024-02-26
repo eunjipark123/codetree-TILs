863dd295b87d717476126ce50a156eaefec7f7ed
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 16:55:24 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240226/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240226/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,21 +1,38 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-cnt = 1
-max_val = arr[0]
-sec_val = arr[0]
+# n = 7
+# arr = [3, 2, 3, 4, 5, 5, 1]
+max_val = 0
+dup_idx = []
 
-for elem in arr[1:]:
-    if elem > max_val:
-        cnt = 1
-        max_val = elem
-        sec_val = max_val
+while True:
+    if max_val != 0:
+        break
+    elif len(dup_idx) == n:
+        max_val = -1
     
-    elif elem == max_val:
-        cnt = 0
-        max_val = sec_val
+    for i in range(n):
+        # 제낄 조건 : 수가 작거나 중복리스트에 들어간 경우
+        if arr[i] < max_val:
+            continue
+        
+        if i in dup_idx:
+            continue
 
-if cnt == 0:
-    print(-1)
-else:
-    print(max_val)
+        # 큰 수가 처음 나오면
+        if arr[i] > max_val:
+            max_val = arr[i]
+            max_idx = []
+            max_idx.append(i)
+        
+        # 같은 수가 나오면
+        elif arr[i] == max_val:
+            max_idx.append(i)
+            max_val = 0
+            
+            for idx in max_idx:
+                dup_idx.append(idx)
+            break
+        
+print(max_val)
