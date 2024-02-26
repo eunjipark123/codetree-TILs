c3437a20c999d011ac59c37891d099bbbe818cbf
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 13:10:43 +0900

    Title: n개의 숫자 중 최대 2개, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240226/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -2,7 +2,6 @@ n = int(input())
 arr = list(map(int, input().split()))
 # 기초적인 방법 : 가장 큰 값을 찾고, 그 값을 제거하고, 나머지에서 가장 큰 값을 찾기
 max_val = arr[0]
-sec_max_val = arr[0]
 
 for elem in arr:
     if elem > max_val:
@@ -12,6 +11,7 @@ for i in range(n):
     if arr[i] == max_val:
         arr[i] = -2**31
 
+sec_max_val = arr[0]
 for elem in arr:
     if elem > sec_max_val:
         sec_max_val = elem
