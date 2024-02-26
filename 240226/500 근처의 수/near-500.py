4cedcee0898328af75511dfb2d4cbdafe3c59d4c
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 16:05:33 +0900

    Title: 500 근처의 수, Time: 174ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240226/500 근처의 수/near-500.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+max_val = 0
+min_val = 1000
+
+for elem in arr:
+    if elem > 500 and elem < min_val:
+        min_val = elem
+    elif elem < 500 and elem > max_val:
+        max_val = elem
+    
+print(max_val, min_val)
