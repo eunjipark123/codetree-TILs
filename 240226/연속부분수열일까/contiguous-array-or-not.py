3d76769575da47a707a763532c034e25d0fcd9fd
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 11:58:39 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240226/연속부분수열일까/contiguous-array-or-not.py
+++ 240226/연속부분수열일까/contiguous-array-or-not.py
@@ -1,7 +1,4 @@
-# 10 6
-# 9 2 4 1 3 3 1 4 6 8
-# 4 1 3 3 1 4
-# 수열 갯수 값 받기
+# a, b 길이값 받기
 ab = input().split()
 len_a, len_b = int(ab[0]), int(ab[1])
 
@@ -9,32 +6,45 @@ len_a, len_b = int(ab[0]), int(ab[1])
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-# 테스트케이스
-# len_a, len_b = 10, 6
-# a = [9, 2, 4, 1, 3, 3, 1, 4, 6, 8]
-# b = [4, 1, 3, 3, 1, 4]
 
-# 원소가 a에 들어있는지 확인하기 위한 flag
-in_bool = True
-# b의 첫 수열이 포함된 a의 인덱스를 idx_first, 마지막 수열이 포함된 a의 인덱스를 idx_last라고 하면
-
-for i in range(len_b):
-    # 들어있지 않다면 연속부분수열일 수 없음
-    if b[i] not in a:
-        in_bool = False
-        break    
-
-if in_bool:
-    idx_first = a.index(b[0])
-    idx_last = a.index(b[0]) + len_b
-    
-    new_a = a[idx_first:idx_last]
-    for idx in range(len_b):
-        if new_a[idx] != b[idx]:
-            in_bool = False
-            break   
-    
-if in_bool == False:
+# b의 처음 원소가 a에 있는지 확인 -> 없다면 No
+if b[0] not in a:
     print("No")
+
+# 있다면 해당 원소가 있는 a의 위치를 찾음
+start_idx = []
 else:
-    print("Yes")
+    start_idx = [idx for idx in range(len_a) if a[idx] == b[0]]
+    
+    
+    for i in start_idx:
+        # 그 위치부터의 원소의 갯수를 세서, b의 원소 갯수보다 적다면 무조건 탈락
+        if len(a[i:]) < len_b:
+            print("No")
+            break
+
+        # 아니라면 a를 해당 길이만큼 슬라이싱해서 두 배열이 같은지 Flag로 확인            
+        new_a = a[i:i+len_b]
+        flags = False
+        
+        if new_a == b:
+            flags = True
+            break
+
+    if flags:
+        print("Yes")
+    else:
+        print("No")
+        
+            
+
+
+        
+    # if in_bool == False:
+    #     print("No")
+    # else:
+    #     print("Yes")
+    # test
+# len_a, len_b = 13, 6
+# a = [1, 2, 5, 2, 5, 2, 3, 2, 5, 2, 5, 2, 5]
+# b = [2, 5, 2, 5, 2, 5]
