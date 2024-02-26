be647bcb2270bc95b7406324f4fc5afad38750df
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-26 22:49:33 +0900

    Title: 대문자로 바꾸기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/대문자로 바꾸기/change-to-capital.py
+++ 240226/대문자로 바꾸기/change-to-capital.py
@@ -1,8 +1,19 @@
-n = 5
+# n = 5
 
-b = ([list(input().split()) for _ in range(n)])
+# b = ([list(input().split()) for _ in range(n)])
 
-for row in b:
-    for i in row:
-        print(chr(ord(i) + (ord('A') - ord('a'))), end = " ")
+# for row in b:
+#     for i in row:
+#         print(chr(ord(i) + (ord('A') - ord('a'))), end = " ")
+#     print()
+
+arr_2d = [
+	list(input().split())
+	for _ in range(5)
+]
+
+for i in range(5):
+    for j in range(3):
+        arr_2d[i][j] = chr(ord(arr_2d[i][j]) + ord('A') - ord('a'))
+        print(arr_2d[i][j], end = " ")
     print()
