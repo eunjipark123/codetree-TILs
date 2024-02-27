8ffc7cdddc405e0bd787fd53692823bca915f2ad
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-02-27 13:17:04 +0900

    Title: 지그재그로 숫자 채우기, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240227/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -0,0 +1,25 @@
+nm = input().split()
+n, m = int(nm[0]), int(nm[1])
+
+arr = [[0 for _ in range(n)] for _ in range(m)]
+cnt = 0
+
+for j in range(m):
+    for i in range(n):
+        if j % 2 == 0:
+            arr[j][i] = cnt
+        else:
+            arr[j][n-1-i] = cnt
+        cnt += 1
+
+
+arr_final = [[0 for _ in range(m)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        arr_final[i][j] = arr[j][i]
+
+for row in arr_final:
+    for elem in row:
+        print(elem, end = " ")
+    print()
