a78bb07a33cdc0da4cd26d27bf2eed60cad416e5
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-01 17:51:27 +0900

    Title: 가장 짧은 문자열, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/가장 짧은 문자열/shortest-string.py
@@ -0,0 +1,20 @@
+a = input()
+b = input()
+c = input()
+
+a = len(a)
+b = len(b)
+c = len(c)
+
+if a > b > c:
+    print(a - c)
+elif a > c > b:
+    print(a - b)
+elif b > a > c:
+    print(b - c)
+elif b > c > a:
+    print(b - a)
+elif c > a > b:
+    print(c - b)
+elif c > b > a:
+    print(c - a)
