7233b2b1528d20c884afca96907812443f8a8711
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-03 00:21:02 +0900

    Title: 문자열 나누기, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/문자열 나누기/divide-string.py
+++ 240303/문자열 나누기/divide-string.py
@@ -1,29 +1,45 @@
+str_all = ""
+
 n = int(input())
 
 string = input().split()
-new_string = ""
-for i in string:
-    new_string += i 
 
-if len(new_string)%5 == 0:
-    n = len(new_string) // 5
+for s in string:
+    str_all += s
+
+leng = len(str_all)    
+
+for i in range(leng):
+    print(str_all[i], end ="")
+    if (i + 1) % 5 == 0:
+        print()
+
+# n = int(input())
+
+# string = input().split()
+# new_string = ""
+# for i in string:
+#     new_string += i 
+
+# if len(new_string)%5 == 0:
+#     n = len(new_string) // 5
     
-    for i in range(n):
-        for j in range(5):
-            print(new_string[i * 5 + j], end = "")
-        print()
+#     for i in range(n):
+#         for j in range(5):
+#             print(new_string[i * 5 + j], end = "")
+#         print()
 
-else:
-    n = len(new_string) // 5 + 1
-    m = len(new_string) % 5
+# else:
+#     n = len(new_string) // 5 + 1
+#     m = len(new_string) % 5
 
-    for i in range(n):
-        if i < n-1:
-            for j in range(5):
-                print(new_string[i * 5 + j], end = "")
+#     for i in range(n):
+#         if i < n-1:
+#             for j in range(5):
+#                 print(new_string[i * 5 + j], end = "")
 
-        else:
-            for j in range(m):
-                print(new_string[i * 5 + j], end = "")
+#         else:
+#             for j in range(m):
+#                 print(new_string[i * 5 + j], end = "")
         
-        print()
+#         print()
