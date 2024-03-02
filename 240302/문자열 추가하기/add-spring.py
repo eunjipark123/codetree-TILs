181d92c843c6d2e692c359c52e3a81f767cfdb73
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-02 23:27:58 +0900

    Title: 문자열 추가하기, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240302/문자열 추가하기/add-spring.py
@@ -0,0 +1,20 @@
+string = input()
+
+new_string = string + "Hello"
+print(new_string)
+
+# a, b, c = "apple", "banana", "candy"
+
+# tot_str = ""
+
+# tot_str += a
+# tot_str += b
+# tot_str += c 
+
+# print(tot_str)
+
+# tot_str = ""
+
+# for target_str in [a, b, c]:
+#     tot_str += target_str
+# print(tot_str)
