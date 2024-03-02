c77e9e5a25d377777c23eb3c8a8783eed1ab93e4
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-03 00:13:17 +0900

    Title: 문자열 나누기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240303/문자열 나누기/divide-string.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+string = input().split()
+new_string = ""
+for i in string:
+    new_string += i 
+
+if len(new_string)%5 == 0:
+    n = len(new_string) // 5
+    
+    for i in range(n):
+        for j in range(5):
+            print(new_string[i * 5 + j], end = "")
+
+
+else:
+    n = len(new_string) // 5 + 1
+    m = len(new_string) % 5
+
+    for i in range(5):
+        if i < n-1:
+            for j in range(n):
+                print(new_string[i * 5 + j], end = "")
+
+        else:
+            for j in range(m):
+                print(new_string[i * 5 + j], end = "")
+        print()
