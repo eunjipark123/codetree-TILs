c3fcfa402486d67d28dc81615b2d71e83c03f2ce
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-02 11:02:10 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 240302/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -1,6 +1,11 @@
 arr = [input() for _ in range(10)]
 letter = input()
+cnt = 0
 
 for elem in arr:
     if elem[-1] == letter:
         print(elem)
+        cnt += 1
+
+if cnt == 0:
+    print("None")
