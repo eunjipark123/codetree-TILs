f052e38cb2b4834efeebd048b259be91657ff329
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-03 01:03:26 +0900

    Title: 특정 문자의 유무, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/특정 문자의 유무/specific-character-presence.py
+++ 240303/특정 문자의 유무/specific-character-presence.py
@@ -4,10 +4,14 @@ len_s = len(string)
 for letter in ['ee', 'ab']:
     exists = False
     
-    for i in range(len_s - 1):
-        if string[i] == letter[0] and string[i + 1] == letter[1]:
-            exists = True
+    # for i in range(len_s - 1):
+    #     if string[i] == letter[0] and string[i + 1] == letter[1]:
+    #         exists = True
         
+    for i in range(len_s - 1):
+        if string[i:i + 2] == letter:
+            exists = True
+    
     if exists == True:
         print("Yes", end = " ")
     else:
