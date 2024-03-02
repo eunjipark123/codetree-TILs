c535604847c1b5d583c0c677162351e925b20620
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-02 11:18:33 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240302/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 240302/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -1,11 +1,24 @@
-# 입력값 받기 
+# # 입력값 받기 
+# string = input()
+# n = int(input())
+
+# # 문자열의 길이 비교
+# if n > len(string):
+#     print(string[::-1])
+# else:
+#     # 끝에서부터 n개까지 출력 
+#     for i in range(n):
+#         print(string[::-1][i], end = "")
+
+
 string = input()
 n = int(input())
 
-# 문자열의 길이 비교
-if n > len(string):
-    print(string[::-1])
-else:
-    # 끝에서부터 n개까지 출력 
-    for i in range(n):
-        print(string[::-1][i], end = "")
+leng = len(string)
+cnt = 0
+
+for i in range(leng -1, -1, -1):
+    if cnt => n:
+        break
+    print(string[i], end = "")
+    cnt += 1
