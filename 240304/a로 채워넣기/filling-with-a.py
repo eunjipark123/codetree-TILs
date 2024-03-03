f5d31ba1954f4669c5d35e4c951a5e3de48f5213
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 01:21:56 +0900

    Title: a로 채워넣기, Time: 111ms, Memory: 30MB, Status: Passed - CodeTree

--- 240304/a로 채워넣기/filling-with-a.py
+++ 240304/a로 채워넣기/filling-with-a.py
@@ -1,4 +1,13 @@
 string = input()
 # 첫 번째 방법 (새롭게 만들어서 결합하기)
-s = string[:1]+'a'+string[2:-2]+'a'+string[-1]
-print(s)
+# s = string[:1]+'a'+string[2:-2]+'a'+string[-1]
+# print(s)
+
+# 두 번째 방법 : list, join
+string = list(string)
+string[1] = 'a'
+string[-2] = 'a'
+
+string = "".join(string)
+
+print(string)
