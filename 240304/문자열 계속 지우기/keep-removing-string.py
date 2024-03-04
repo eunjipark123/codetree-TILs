ae3c03f31470b76a8bdc7342d6b8e28b9e5b38f5
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 21:36:30 +0900

    Title: 문자열 계속 지우기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/문자열 계속 지우기/keep-removing-string.py
+++ 240304/문자열 계속 지우기/keep-removing-string.py
@@ -1,17 +1,26 @@
+# # 문자열 입력 
+# a = input()
+# b = input()
+# len_b = len(b)
+
+# while True:   
+#     flag = False
+#     for i in range(len(a)):
+#         if a[i:i + len_b] == b:            
+#             flag = True
+#             a = a[:i] + a[i+len_b:]
+                            
+#     if not flag:
+#         print(a)
+#         break
+
+
 # 문자열 입력 
 a = input()
 b = input()
-len_a = len(a)
-len_b = len(b)
 
+while a.find(b) != -1:
+    start_idx = a.find(b)
+    a = a[:start_idx] + a[start_idx + len(b):]
 
-while True:   
-    flag = False
-    for i in range(len(a)):
-        if a[i:i + len_b] == b:            
-            flag = True
-            a = a[:i] + a[i+len_b:]
-                            
-    if not flag:
-        print(a)
-        break
+print(a)
