ac4747bd04dc55b7c9a1b76ed190c63ad6af4d63
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 00:30:45 +0900

    Title: 반복 출력하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240305/반복 출력하기/repeated-output.py
@@ -0,0 +1,16 @@
+def print_special_chars(n):
+    for _ in range(n):
+        print("12345^&*()_")
+
+n = int(input())
+print_special_chars(n)
+
+
+
+# def print_n_lines(n):
+#     for _ in range(n):
+#         print("*" * 5)
+
+
+# row_num = 4
+# print_n_lines(row_num)
