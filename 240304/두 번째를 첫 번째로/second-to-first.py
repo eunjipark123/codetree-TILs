a66100d5dc1b8a50cbd29e6f21e22fb30f03ad57
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 20:24:59 +0900

    Title: 두 번째를 첫 번째로, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/두 번째를 첫 번째로/second-to-first.py
+++ 240304/두 번째를 첫 번째로/second-to-first.py
@@ -2,8 +2,14 @@ string = input()
 fst = string[0]
 snd = string[1]
 
-for letter in string:
-    if letter == snd:
-        print(fst, end = "")
-    else:
-        print(letter, end = "")
+# for letter in string:
+#     if letter == snd:
+#         print(fst, end = "")
+#     else:
+#         print(letter, end = "")
+
+for i in range(len(string)):
+    if string[i] == snd:
+        string = string[:i] + fst + string[i + 1:]
+        
+print(string)
