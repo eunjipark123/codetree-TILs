e3eb6c1d0364d9fae9d2e8068dfb2aa88f7b2cf9
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 21:39:55 +0900

    Title: e 제거하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/e 제거하기/e-to-remove.py
+++ 240304/e 제거하기/e-to-remove.py
@@ -1,8 +1,20 @@
 string = input()
 
-for i in range(len(string)):
-    if string[i] == 'e':
-        result = string[:i] + string[i+1:]
+# list로 풀기
+arr = list(string)
+leng = len(arr)
+
+for i in range(leng):
+    if arr[i] == 'e':
+        arr.pop(i)
         break
 
-print(result)
+string = ''.join(arr)
+print(string)
+# # slicing으로 풀기 
+# for i in range(len(string)):
+#     if string[i] == 'e':
+#         result = string[:i] + string[i+1:]
+#         break
+
+# print(result)
