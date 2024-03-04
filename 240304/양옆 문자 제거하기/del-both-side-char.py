368a4e36c66bc967287055febba6430b1aedcccc
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 20:29:20 +0900

    Title: 양옆 문자 제거하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240304/양옆 문자 제거하기/del-both-side-char.py
@@ -0,0 +1,19 @@
+string = input()
+# slicing을 이용한 방법
+string = string[:1] + string[2:-2] + string[-1:]
+print(string)
+
+# 기본 개념
+# # list를 이용한 방법
+# s = 'baknana'
+# arr = list(s)
+# arr.pop(2)
+# s = "".join(arr)
+# print(s)
+
+# # slicing을 이용한 방법 
+# s = 'baknana'
+# print(s)
+# print()
+# s = s[:2] + s[3:]
+# print(s)
