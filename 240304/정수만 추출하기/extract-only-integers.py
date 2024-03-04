141d25f3f48099c42a9165cb1bf64be40033c46b
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 23:03:09 +0900

    Title: 정수만 추출하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/정수만 추출하기/extract-only-integers.py
+++ 240304/정수만 추출하기/extract-only-integers.py
@@ -1,23 +1,53 @@
+# 인덱스로 푸는 방식
 a, b = input().split()
 
+idx1 = 0
+idx2 = 0
 
-new_a = ""
-new_b = ""
+# a
+for elem in a:
+    if elem <= '9' and elem >= '0':
+        idx1 += 1
+    else:
+        break
+
+# b
+for elem in b:
+    if elem <= '9' and elem >= '0':
+        idx2 += 1
+    else:
+        break
+
+str1 = a[:idx1]
+str2 = b[:idx2]
+
+#숫자화
+str1 = int(str1)
+str2 = int(str2)
+
+# 합계
+print(str1 + str2)
+
+# a, b = input().split()
+
+
+# new_a = ""
+# new_b = ""
  
-for char in a:
-    if not char.isdecimal():
-        break
+# for char in a:
+#     if not char.isdecimal():
+#         break
     
-    new_a += char
+#     new_a += char
 
 
-for char in b:
-    if not char.isdecimal():
-        break
+# for char in b:
+#     if not char.isdecimal():
+#         break
     
-    new_b += char
+#     new_b += char
 
-new_a = int(new_a)
-new_b = int(new_b)
+# new_a = int(new_a)
+# new_b = int(new_b)
 
-print(new_a + new_b)
+# print(new_a + new_b)
