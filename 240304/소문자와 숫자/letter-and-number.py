a3ebb4a235a2edb6b96e8e379726dc8c7f050d65
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 22:39:16 +0900

    Title: 소문자와 숫자, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240304/소문자와 숫자/letter-and-number.py
+++ 240304/소문자와 숫자/letter-and-number.py
@@ -1,3 +1,6 @@
 string = input()
-string = "".join(char.lower() for char in string if char.isnumeric() or char.isalpha())
-print(string)
+# string = "".join(char.lower() for char in string if char.isnumeric() or char.isalpha())
+
+for char in string:
+    if char.isalpha() or char.isnumeric():
+        print(char.lower(), end = "")
