f6d99d5b9fe675102b490411123a4d0818159526
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-04 18:11:58 +0900

    Title: 문자 교체하기, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/문자 교체하기/changing-char.py
+++ 240304/문자 교체하기/changing-char.py
@@ -1,3 +1,10 @@
-arr = list(map(list, input().split()))
-arr[1][:2] = arr[0][:2]
-print("".join(arr[1]))
+# arr = list(map(list, input().split()))
+# arr[1][:2] = arr[0][:2]
+# print("".join(arr[1]))
+
+
+str1, str2 = input().split()
+
+str2 = str1[:2] + str2[2:]
+
+print(str2)
