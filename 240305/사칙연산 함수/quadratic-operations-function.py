05df4a3490b0824a5e2806381c856b035bcf1bfc
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 10:16:54 +0900

    Title: 사칙연산 함수, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/사칙연산 함수/quadratic-operations-function.py
+++ 240305/사칙연산 함수/quadratic-operations-function.py
@@ -1,29 +1,29 @@
 a, o, c = input().split()
 a, c = int(a), int(c)
 
-def plus(a, o, c):
+def plus(a, c):
     return a + c
 
-def minus(a, o, c):
+def minus(a, c):
     return a - c
 
-def devide(a, o, c):
+def devide(a, c):
     return a // c
 
-def multiple(a, o, c):
+def multiple(a, c):
     return a * c    
 
 if o == "+":
-    print(f"{a} + {c} = {plus(a, o, c)}")
+    print(f"{a} + {c} = {plus(a, c)}")
 
 elif o == "-":
-    print(f"{a} - {c} = {minus(a, o, c)}")
+    print(f"{a} - {c} = {minus(a, c)}")
 
 elif o == "*":
-    print(f"{a} * {c} = {multiple(a, o, c)}")    
+    print(f"{a} * {c} = {multiple(a, c)}")    
 
 elif o == "/":
-    print(f"{a} / {c} = {devide(a, o, c)}")
+    print(f"{a} / {c} = {devide(a, c)}")
 
 else:
     print(False)
