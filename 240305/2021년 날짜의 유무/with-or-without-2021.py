3fccb8695fddcbcc55f594d88bd979b434880f30
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 11:22:33 +0900

    Title: 2021년 날짜의 유무, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/2021년 날짜의 유무/with-or-without-2021.py
+++ 240305/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,25 +1,44 @@
 m, d = tuple(map(int, input().split()))
-def date_by_month_2021(m):
-    if m > 12:
-        date = []
+
+def last_day_number(m):
+    if m == 2:
+        return 28
+    if m in (4, 6, 9, 11):
+        return 30
+    return 31
+
+def judge_date(m, d):
+    if m <= 12 and d <= last_day_number(m):
+        return True
+    return False
+
+if judge_date(m, d):
+    print("Yes")
+else:
+    print("No")
+
+# m, d = tuple(map(int, input().split()))
+# def date_by_month_2021(m):
+#     if m > 12:
+#         date = []
     
-    elif m in (1, 3, 5, 7, 8, 10, 12):
-        date = [i for i in range(1, 32)]
+#     elif m in (1, 3, 5, 7, 8, 10, 12):
+#         date = [i for i in range(1, 32)]
     
-    elif m == 2:
-        date = [i for i in range(1, 29)]
+#     elif m == 2:
+#         date = [i for i in range(1, 29)]
     
-    else:
-        date = [i for i in range(1, 31)]
+#     else:
+#         date = [i for i in range(1, 31)]
 
-    return date
+#     return date
 
-def is_date_exsist_in_2021(m, d):
-    if d in date_by_month_2021(m):
-        answer = 'Yes'
-    else:
-        answer = 'No'
+# def is_date_exsist_in_2021(m, d):
+#     if d in date_by_month_2021(m):
+#         answer = 'Yes'
+#     else:
+#         answer = 'No'
 
-    return answer
+#     return answer
 
-print(is_date_exsist_in_2021(m, d))
+# print(is_date_exsist_in_2021(m, d))
