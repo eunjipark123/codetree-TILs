26e584ef8dccab344823a0b8f05928dbd3c6e4d2
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 09:53:49 +0900

    Title: 두 수의 거듭제곱, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/두 수의 거듭제곱/two-numbers-of-squares.py
+++ 240305/두 수의 거듭제곱/two-numbers-of-squares.py
@@ -1,7 +1,15 @@
 a, b = tuple(map(int, input().split()))
 
 def return_power(a, b):
-    return a**b
+#     return a**b
+
+
+
+    cnt = 1
+    for _ in range(b):
+        cnt *= a
+    
+    return cnt
 
 
 print(return_power(a, b))
