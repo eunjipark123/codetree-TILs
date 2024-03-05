2f3c3145d43e3049b5db498d8185babeb2e7d3df
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 15:08:20 +0900

    Title: 특정 구간의 합, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240305/특정 구간의 합/sum-of-specific-intervals.py
+++ 240305/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,11 +1,19 @@
 n, m = tuple(map(int, input().split()))
 a = [0] + list(map(int, input().split()))
 
-def section_sum_val(m):
-    global a
+# def section_sum_val(m):
+#     global a
     
-    for _ in range(m):
-        a1, a2 = tuple(map(int, input().split()))
-        print(sum(a[a1:a2+1]))
+#     for _ in range(m):
+#         a1, a2 = tuple(map(int, input().split()))
+#         print(sum(a[a1:a2+1]))
 
-section_sum_val(m)
+# section_sum_val(m)
+
+
+def section_sum_val(a1, a2):
+    return sum(a[a1:a2+1])
+
+for _ in range(m):
+    a1, a2 = tuple(map(int, input().split()))
+    print(section_sum_val(a1, a2))
