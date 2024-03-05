9df653dff0d734252d2cba6b92de9b7dc29ff7e1
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 10:15:54 +0900

    Title: 사칙연산 함수, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/사칙연산 함수/quadratic-operations-function.py
+++ 240305/사칙연산 함수/quadratic-operations-function.py
@@ -1,20 +1,48 @@
 a, o, c = input().split()
 a, c = int(a), int(c)
 
-def basic_operations(a, o, c):
-    if o not in ('+', '-', '/', '*'):
-        print(False)
+def plus(a, o, c):
+    return a + c
+
+def minus(a, o, c):
+    return a - c
+
+def devide(a, o, c):
+    return a // c
+
+def multiple(a, o, c):
+    return a * c    
+
+if o == "+":
+    print(f"{a} + {c} = {plus(a, o, c)}")
+
+elif o == "-":
+    print(f"{a} - {c} = {minus(a, o, c)}")
+
+elif o == "*":
+    print(f"{a} * {c} = {multiple(a, o, c)}")    
+
+elif o == "/":
+    print(f"{a} / {c} = {devide(a, o, c)}")
+
+else:
+    print(False)
+
+# 틀린 풀이 
+# def basic_operations(a, o, c):
+#     if o not in ('+', '-', '/', '*'):
+#         print(False)
     
-    if o == '+':
-        print(f"{a} + {c} = {a + c}")
+#     if o == '+':
+#         print(f"{a} + {c} = {a + c}")
     
-    elif o == '-':
-        print(f"{a} - {c} = {a - c}")
+#     elif o == '-':
+#         print(f"{a} - {c} = {a - c}")
 
-    elif o == '/':
-        print(f"{a} / {c} = {a // c}")
+#     elif o == '/':
+#         print(f"{a} / {c} = {a // c}")
 
-    elif o == '*':
-        print(f"{a} * {c} = {a * c}")
+#     elif o == '*':
+#         print(f"{a} * {c} = {a * c}")
 
-basic_operations(a, o, c)
+# basic_operations(a, o, c)
