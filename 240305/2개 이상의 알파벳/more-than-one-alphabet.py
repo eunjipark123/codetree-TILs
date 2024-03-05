154fd1f6bd17250b22d3329cd61dd2931a03cdcb
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 13:05:10 +0900

    Title: 2개 이상의 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240305/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,18 @@
+# 입력
+a = input()
+
+def judge_more_than_2_alpha(a):
+    if len(a) == 1:
+        return False
+    
+    for i in range(1, len(a)+1):
+        if a[i] not in a[:i]:
+            return True    
+    
+    return False 
+
+    
+if judge_more_than_2_alpha(a):
+    print('Yes')
+else:
+    print('No')
