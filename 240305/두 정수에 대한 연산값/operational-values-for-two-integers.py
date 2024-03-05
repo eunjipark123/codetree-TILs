8753f62a02e3f6413d44e2c73fb73f630987b12a
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 12:44:58 +0900

    Title: 두 정수에 대한 연산값, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240305/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,8 +1,13 @@
 a, b = tuple(map(int, input().split()))
 
 def calcuator(a, b):
-    b += 25
-    a *= 2
+    if a > b:
+        a += 25
+        b *= 2
+    
+    else:
+        a *= 2
+        b += 25
     
     return a, b
 
