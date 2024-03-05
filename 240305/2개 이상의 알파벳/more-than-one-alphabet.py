dd8087491876bdc5b42f08b4340646aa74b325da
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 13:06:20 +0900

    Title: 2개 이상의 알파벳, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240305/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -5,7 +5,7 @@ def judge_more_than_2_alpha(a):
     if len(a) == 1:
         return False
     
-    for i in range(1, len(a)+1):
+    for i in range(1, len(a)):
         if a[i] not in a[:i]:
             return True    
     
