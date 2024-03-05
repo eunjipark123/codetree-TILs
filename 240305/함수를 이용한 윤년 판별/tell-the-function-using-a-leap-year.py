3711579b338538b2e32086da57ea207b5d907f3a
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 09:22:50 +0900

    Title: 함수를 이용한 윤년 판별, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240305/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,26 @@
+y = int(input())
+
+def is_leap_year(y):
+    if y % 4 != 0:
+        return False
+    
+    if (y % 100) == 0 and (y % 400) != 0 :
+        return False
+    
+    return True
+
+print("true") if is_leap_year(y) else print("false")
+# def is_perfect_number(n):
+#     if n % 2 == 0:
+#         return False
+#     if n % 10 == 5:
+#         return False
+#     if n % 3 == 0 and n % 9 != 0:
+#         return False
+    
+#     return True
+
+# if is_perfect_number(9):
+#     print("Y")
+# else:
+#     print("N")
