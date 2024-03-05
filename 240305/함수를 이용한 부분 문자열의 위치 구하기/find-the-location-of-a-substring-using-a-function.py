86c81f68ec2b23b2f01c1876f312e9dfebb0b1e9
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 14:27:02 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240305/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -2,14 +2,35 @@ input_str = input()
 target_str = input()
 
 
-def idx_finder():    
-    idx = -1
-    for i in range(len(input_str)):
-        if input_str[i:i + len(target_str)] == target_str:
-            idx = i
-            break
+def is_substr(start_idx):
+    n, m = len(input_str), len(target_str)
 
-    return idx
+    if start_idx + m - 1 > n:
+        return False
+    
+    for j in range(m):
+        if input_str[start_idx + j] != target_str[j]:
+            return False
+
+    return True
+
+def find_index():
+    n = len(input_str)
+    for i in range(n):
+        if is_substr(i):
+            return i
+
+    return -i        
+
+print(find_index())
+# def idx_finder():    
+#     idx = -1
+#     for i in range(len(input_str)):
+#         if input_str[i:i + len(target_str)] == target_str:
+#             idx = i
+#             break
+
+#     return idx
     
 
-print(idx_finder())
+# print(idx_finder())
