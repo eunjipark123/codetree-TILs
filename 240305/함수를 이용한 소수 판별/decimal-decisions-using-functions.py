859301cca73d24a61517260d99601c3166f0cc23
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 09:17:18 +0900

    Title: 함수를 이용한 소수 판별, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240305/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,9 +1,13 @@
 a, b = tuple(map(int, input().split()))
 
 def is_prime(n):
+    if n == 1:
+        return False
+    
     for i in range(2, n):
         if n % i == 0:
-            return False
+            return False    
+
     return True
 
 def prime_sum(a, b):
