535da80d02fd17e32de755a6e2b680a69ea3792d
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-05 12:42:38 +0900

    Title: 두 정수에 대한 연산값, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240305/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,13 +1,9 @@
 a, b = tuple(map(int, input().split()))
 
 def calcuator(a, b):
-    big_num = max(a, b)
-    small_num = min(a, b)
-
-    big_num += 25
-    small_num *= 2
-
-    return small_num, big_num
+    b += 25
+    a *= 2
+    return a, b
 
 a, b = calcuator(a, b)
 print(a, b)
