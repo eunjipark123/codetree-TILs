7d74072fa7fd1fb375fa49e0bd6ad7866865aeb6
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-07 01:47:49 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def last_common_multiple(n):
+    global arr
+    
+    if n == 1:
+        return 1
+
+    lastone = last_common_multiple(n-1)
+    
+    # 새로운 수의 약수
+    for i in range(arr[n-1], 0, -1):
+        if arr[n-1] % i == 0 and lastone % i == 0:
+            return lastone // i * arr[n-1] 
+
+        
+print(last_common_multiple(n))
