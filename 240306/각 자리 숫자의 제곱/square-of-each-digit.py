bd7a193fbb2c1b118d38436eb729b33534528d3e
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-06 15:00:52 +0900

    Title: 각 자리 숫자의 제곱, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240306/각 자리 숫자의 제곱/square-of-each-digit.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def f(n):
+    if n < 10:
+        return n**2
+
+    return f(n // 10) + (n % 10)**2
+
+print(f(n))
+# n = 1527
+
+# def f(n):
+#     if n < 10:
+#         return n
+    
+#     return f(n // 10) + (n % 10)
+    
+
+# print(f(n))
