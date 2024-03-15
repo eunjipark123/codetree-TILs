fcb77261c4fe57344cded2ec3f47bfa158586671
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-15 16:47:18 +0900

    Title: Next Level, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/Next Level/next-level.py
+++ 240315/Next Level/next-level.py
@@ -1,14 +1,21 @@
-user = input().split()
-user_id, user_lv = user
-user_lv = int(user_lv)
+# user = input().split()
+# user_id, user_lv = user
+# user_lv = int(user_lv)
 
-class User:
-    def __init__(self, user_id = 'codetree', user_lv = 10):
-        self.user_id = user_id
-        self.user_lv = user_lv
+# class User:
+#     def __init__(self, user_id = 'codetree', user_lv = 10):
+#         self.user_id = user_id
+#         self.user_lv = user_lv
 
-user0 = User()
-user1 = User(user_id, user_lv)
+# user0 = User()
+# user1 = User(user_id, user_lv)
 
-print(f"user {user0.user_id} lv {user0.user_lv}")
-print(f"user {user1.user_id} lv {user1.user_lv}")
+# print(f"user {user0.user_id} lv {user0.user_lv}")
+# print(f"user {user1.user_id} lv {user1.user_lv}")
+    
+
+user1 = ("codetree", 10)
+user2 = tuple(input().split())
+
+for user_id, user_level in [user1, user2]:
+    print(f"user {user_id} lv {user_level}")
