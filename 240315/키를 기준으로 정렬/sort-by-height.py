0e7d34373c33ced81fc2877909a044074b510b28
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-15 22:53:39 +0900

    Title: 키를 기준으로 정렬, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/키를 기준으로 정렬/sort-by-height.py
+++ 240315/키를 기준으로 정렬/sort-by-height.py
@@ -1,8 +1,26 @@
+# 클래스로 풀어보기 
+class Student:
+    def __init__(self, name, h, w):
+        self.name = name 
+        self.h = h
+        self.w = w
+
+    def print_it(self):
+        print(self.name, self.h, self.w)
+
+
+# 빈 리스트 생성
+students = []
+
+# 입력 받은 값을 리스트에 넣기
 n = int(input())
+for _ in range(n):
+    name, h, w = input().split()
+    students.append(Student(name, int(h), int(w)))
 
-# 튜플로 풀어보기
-students = [tuple(input().split()) for _ in range(n)]
-students.sort(key=lambda x: x[1])
+# 정렬 
+students.sort(key=lambda x: x.h)
 
-for name, h, w in students:
-    print(name, h, w)
+# 출력
+for elem in students:
+    elem.print_it()
