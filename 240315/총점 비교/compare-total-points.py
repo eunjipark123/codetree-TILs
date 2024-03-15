6fbfacd4e6f021242eb4e0d7f3fcb19b1d28dc85
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-15 23:27:08 +0900

    Title: 총점 비교, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/총점 비교/compare-total-points.py
+++ 240315/총점 비교/compare-total-points.py
@@ -1,25 +1,9 @@
-# class
-class Score:
-    def __init__(self, name, a, b, c):
-        self.name = name
-        self.a = a
-        self.b = b
-        self.c = c
+# tuple
 
-    def print_score(self):
-        print(self.name, self.a, self.b, self.c)
-
-# 리스트 채우기
 n = int(input())
-scores = []
+scores = [tuple(input().split()) for _ in range(n)]
 
-for _ in range(n):
-    name, a, b, c = input().split()
-    scores.append(Score(name, int(a), int(b), int(c)))
+scores.sort(key=lambda x: int(x[1]) + int(x[2]) + int(x[3]))
 
-# 정렬 
-scores.sort(key=lambda x: x.a + x.b + x.c)
-
-# 출력 
-for score in scores:
-    score.print_score()
+for name, a, b, c in scores:
+    print(name, a, b, c)
