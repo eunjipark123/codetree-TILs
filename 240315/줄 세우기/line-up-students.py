785da738e23afc6c7b036af5576d1a50fba603ce
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-15 23:50:34 +0900

    Title: 줄 세우기, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 240315/줄 세우기/line-up-students.py
+++ 240315/줄 세우기/line-up-students.py
@@ -7,7 +7,7 @@ for k in range(1, n+1):
     h, w = map(int, input().split())
     students.append((k, h, w))
 
-students.sort(key=lambda x: (-x[1], -x[2], -x[0]))
+students.sort(key=lambda x: (-x[1], -x[2], x[0]))
 
 for k, h, w in students:
     print(h, w, k)
