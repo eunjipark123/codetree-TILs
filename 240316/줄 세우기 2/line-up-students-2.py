f77e9ffb558d0b55756b1b604ee87b17a6924da7
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-16 00:28:55 +0900

    Title: 줄 세우기 2, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 240316/줄 세우기 2/line-up-students-2.py
+++ 240316/줄 세우기 2/line-up-students-2.py
@@ -12,11 +12,14 @@ class Student:
 n = int(input())
 students = []
 
+# 리스트에 넣어두기 
 for num in range(1, n + 1):
     h, w = tuple(map(int, input().split()))
     students.append(Student(h, w, num))
 
+# 정렬 
 students.sort(key=lambda x: (x.h, -x.w))
 
+# 출력
 for student in students:
     student.print_student()
