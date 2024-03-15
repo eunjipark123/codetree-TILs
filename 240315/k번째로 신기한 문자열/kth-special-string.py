5e2b23530e1a14256fc915a2c9d25add9efce2b6
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-15 15:12:14 +0900

    Title: k번째로 신기한 문자열, Time: 177ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240315/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,17 @@
+n, k, t = input().split()
+n = int(n)
+k = int(k)
+lent = len(t)
+arr = []
+
+# 문자 일치 확인 -> 일치하면 빈리스트에 추가
+for _ in range(n):
+    string = input()
+    if string[:lent] == t:
+        arr.append(string)
+
+# 빈 리스트를 순서대로 정렬
+arr = sorted(arr)
+
+# k 번째 단어 출력
+print(arr[k-1])
