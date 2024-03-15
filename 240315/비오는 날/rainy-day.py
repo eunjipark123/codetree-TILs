9509f614b6f5f45dc04dbc7ef2044c30193ad4af
박은지 <88650190+eunjipark123@users.noreply.github.com>
2024-03-15 19:23:09 +0900

    Title: 비오는 날, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/비오는 날/rainy-day.py
+++ 240315/비오는 날/rainy-day.py
@@ -7,19 +7,14 @@ class Forecast:
     def print_it(self):
         print(self.date, self.day, self.w)
 
-
 n = int(input())
-rainy_days = []
-
+ans = Forecast("9999-99-99", "", "")
 for _ in range(n):
-    date, day, weather = input().split()
-    if weather == 'Rain':
-        rainy_days.append(Forecast(date, day, weather))
+    date, day, weather = tuple(input().split())
 
+    f = Forecast(date, day, weather)
+    if weather == "Rain":
+        if ans.date >= f.date:
+            ans = f
 
-min_idx = 0
-for i in range(1, len(rainy_days)):
-    if rainy_days[min_idx].date > rainy_days[i].date:
-        min_idx = i
-
-rainy_days[min_idx].print_it()
+ans.print_it()
